497c2228db7c4c443378668c04ece69523d6257b
yasinovec <67464071+yasinovec@users.noreply.github.com>
2021-04-21 16:26:59 +0200

    Добавил ДЗ№1

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+//DZ#0
+
 const TEST_CONSTANTA = 'asd';
 
 $user_name = 'Igor';
@@ -7,3 +9,11 @@ require_once 'function.php';
 if (1 == 1) {
     echo 'hi';
 }
+
+//DZ#1
+
+$name = 'Валентин';
+$age = '35';
+echo '"Меня зовут: '.$name.'"<br>';
+echo 'Мне '.$age.' лет<br>';
+echo '“!|/’”\\';
